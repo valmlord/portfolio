25d6fc44d4a41c3da283dfdc01b13d31488ad2b6
Stepan Korshukov <korshukovstepan@gmail.com>
2023-12-03 22:24:55 +0300

    feat: add mockup and description for the first project

--- components/Project.tsx
+++ components/Project.tsx
@@ -3,6 +3,7 @@
 import { useRef } from "react";
 
 import Image from "next/image";
+import Link from "next/link";
 
 import { projectsData } from "@/lib/data";
 
@@ -12,6 +13,7 @@ import { motion, useScroll, useTransform } from "framer-motion";
 
 const Project = ({
   title,
+  link,
   description,
   tags,
   imageUrl,
@@ -35,13 +37,13 @@ const Project = ({
       }}
       className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 ">
+      <Link href={link} className="flex bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 ">
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold dark:text-gray-700">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700">
             {description}
           </p>
-          <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+          <ul className="flex flex-wrap mt-4 gap-2">
             {tags.map((tag, index) => (
               <li
                 className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full"
@@ -70,7 +72,7 @@ const Project = ({
 
         group-even:right-[initial] group-even:-left-40"
         />
-      </section>
+      </Link>
     </motion.div>
   );
 }
--- lib/data.ts
+++ lib/data.ts
@@ -4,9 +4,7 @@ import { CgWorkAlt } from "react-icons/cg";
 import { FaPhp } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import taskifyImg from "@/public/taskify.png";
 
 export const links = [
   {
@@ -64,26 +62,13 @@ export const experiencesData = [
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "Taskify",
+    link: "https://taskify.korshukovstepan.dev",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
-  },
-  {
-    title: "rmtDev",
-    description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
-  },
-  {
-    title: "Word Analytics",
-    description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
-  },
+      "Optimize your productivity with my task manager – a stylish Trello clone. Create boards, move tasks, manage projects easily and efficiently.",
+    tags: ["React", "Next.js", "Typescript", "MySQL", "Tailwind", "Prisma"],
+    imageUrl: taskifyImg,
+  }
 ] as const;
 
 export const skillsData = [
